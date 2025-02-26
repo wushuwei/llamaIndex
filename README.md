66f01eec3aa5c960758cffa5d529225e39495f1d
wushuwei@gmail.com <wushuwei@gmail.com>
2025-02-26 17:17:10 -0500

    point to local embedding to save token

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # llamaIndex
 # to get the local llm
 # git clone https://huggingface.co/EleutherAI/gpt-neo-1.3B
+# export OPENAI_API_KEY="testing api key"
+# if use embbeded, unset OPENAI_API_KEY
--- quickStart.py
+++ quickStart.py
@@ -1,17 +1,23 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
 
-# Load the local LLM
-model_name = "EleutherAI/gpt-neo-1.3B"
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# global default
+# By default, LlamaIndex will use text-embedding-ada-002
+Settings.embed_model = OpenAIEmbedding()
+
+# Settings.embed_model = HuggingFaceEmbedding(
+#     model_name="BAAI/bge-small-en-v1.5"
+# )
 
 
 documents = SimpleDirectoryReader("~/Data").load_data()
+
 index = VectorStoreIndex.from_documents(documents)
 
-# Configure the query engine to use the local LLM
-query_engine = index.as_query_engine(model=model, tokenizer=tokenizer)
-# query_engine = index.as_query_engine()
-response = query_engine.query("What is the name hometown of Steven Wu?")
+query_engine = index.as_query_engine()
+
+response = query_engine.query("query string")
+
 print(response)
